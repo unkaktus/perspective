9cfc62c2aba4ed5b219fea4fd0d23ef0af17f683
Ivan Markin <git@unkaktus.art>
2024-03-10 00:34:30 +0100

    Fully automated bitant reflection

--- reflect_bitant.py
+++ reflect_bitant.py
@@ -1,40 +1,109 @@
 #!/usr/bin/env python3
 
 # A tool to reflect data in XDMF files along z-direction
-# (Supports only HDF5 for now)
 #
 # Ivan Markin, @unkaktus 2023
-
-import numpy as np
-import h5py
 
 import argparse
 import os
 
+import numpy as np
+import h5py
+import xml.dom.minidom as md
+
+def string_to_floatlist(s):
+    return [float(x) for x in s.split(' ')]
+
+def string_to_intlist(s):
+    return [int(x) for x in s.split(' ')]
+
+def floatlist_to_string(fl):
+    return ' '.join([str(f) for f in fl])
+
 parser = argparse.ArgumentParser('reflect_bitant')
-parser.add_argument("-i", type=str, help="Input filename")
-parser.add_argument("-o", type=str, help="Output filename")
-parser.add_argument("--ghost-points")
+parser.add_argument("-i", type=str, help="Input filename without extension")
+parser.add_argument("--ghost-points", type=int, default=6, help="Number of ghost points")
 args = parser.parse_args()
 
 
-ghost_points = 6
+if __name__ == "__main__":
+    outfilename = args.i+'.reflected'
+    ghost_points = args.ghost_points
+
+    dom = md.parse(args.i+'.xmf')
+    root = dom.documentElement
+
+    ### Reflect XMF file
+    print('[i] Reflecting XMF file')
+    times = {}
+
+    if root.childNodes:
+        for domain_node in root.childNodes:
+            if domain_node.nodeType != domain_node.ELEMENT_NODE:
+                continue
+            for collection_node in domain_node.childNodes:
+                if collection_node.nodeType != collection_node.ELEMENT_NODE:
+                    continue
+                for grid_node in collection_node.childNodes:
+                    if grid_node.nodeType != grid_node.ELEMENT_NODE:
+                        continue
+                    # Filter out duplicate times
+                    time = grid_node.getElementsByTagName("Time")[0].attributes["Value"].value
+                    if time in times:
+                        collection_node.removeChild(grid_node)
+                    else:
+                        times[time] = True
+
+                    topology = grid_node.getElementsByTagName("Topology")[0]
+                    dimensions_text = topology.attributes["Dimensions"]
+                    dimensions = string_to_intlist(dimensions_text.value)
+                    dimensions[0] = (dimensions[0]-ghost_points)*2
+                    dimensions_text.value = floatlist_to_string(dimensions)
+
+                    attribute_node = grid_node.getElementsByTagName("Attribute")[0]
+                    data_item = attribute_node.getElementsByTagName("DataItem")[0]
+                    dimensions_text = data_item.attributes["Dimensions"]
+                    dimensions = string_to_intlist(dimensions_text.value)
+                    dimensions[0] = (dimensions[0]-ghost_points)*2
+                    dimensions_text.value = floatlist_to_string(dimensions)
+
+                    data_pointer = data_item.childNodes[0]
+                    data_item_text = data_pointer.data
+                    data_id = data_item_text.split(":")[1]
+                    data_pointer.data = f'{outfilename}.h5:{data_id}'
 
 
-input_file = h5py.File(args.i, 'r+')
+                    geometry = grid_node.getElementsByTagName("Geometry")[0]
 
-output_file = h5py.File(args.o, 'w')
+                    # Spacings
+                    dx_text = geometry.getElementsByTagName("DataItem")[1].childNodes[0]
+                    dx = string_to_floatlist(dx_text.data)
+                    dz = dx[0]
 
-for key_n, key in enumerate(input_file.keys()):
-    print(f"Handling key {key_n}/{len(input_file.keys())}")
-    data = np.array(input_file[key])
+                    # Origin
+                    origin_text = geometry.getElementsByTagName("DataItem")[0].childNodes[0]
+                    origin = string_to_floatlist(origin_text.data)
+                    origin[0] = -dz*(dimensions[0]//2-0.5)
+                    origin_text.data = floatlist_to_string(origin)
 
-    data = data[ghost_points:,:,:]
-    data_reflected = np.flip(data, axis=0)
-    data = np.concatenate((data_reflected, data), axis=0)
+    dom.writexml(open(outfilename+'.xmf', "w"))
 
-    output_file[key] = data
-    print(f'Output shape: {data.shape}')
+    ### Reflect HDF5 file
+    print('[i] Reflecting HDF5 file')
+    input_file = h5py.File(args.i+'.h5', 'r+')
+    output_file = h5py.File(outfilename+'.h5', 'w')
 
-input_file.close()
-output_file.close()
+    for key_n, key in enumerate(input_file.keys()):
+        print(f"Reflecting timestep {key_n}/{len(input_file.keys())}")
+        data = np.array(input_file[key])
+
+        data = data[ghost_points:,:,:]
+        data_reflected = np.flip(data, axis=0)
+        data = np.concatenate((data_reflected, data), axis=0)
+
+        output_file[key] = data
+
+    input_file.close()
+    output_file.close()
+
+    print('[+] Done.')
